69509d046f0d128245266442e9381af30565567a
Oscar <osbyrne@skiff.com>
2024-03-09 10:17:04 +0000

    commented details of the parser

--- src/scheduler.py
+++ src/scheduler.py
@@ -5,7 +5,7 @@
 part 1: reading constrain tables
 
 according to the subject:
-On each line, the first number is the task number, 
+On each line, the first number is the task number (note that we renamed this attribute to task_id in the class Task, as it is effectively the only identifier of the task in the context of the problem and everything is already a number), 
 the second is its duration, 
 and the other numbers (if present) are the constraints (predecessors)'
 """
@@ -20,14 +20,22 @@ class Task:
         return f"Task number: {self.task_number}, Duration: {self.duration}, Predecessors: {self.predecessors}"
 
 def tasks_from_constraints(filename: str) -> list[Task]:
+    """
+    we assume that:
+    - there is no repetition of tasks (whose id is task_id)
+    - there are only numbers, no letters
+
+    we note that task_id is unique, so we can use it as key in a dictionary
+    we note that task_id is very often equal to the line number, albeit more explicit and forgiving than using the line number as task_id
+    """
     tasks = []
     with open(filename) as file:
         for line in file:
             line = line.split()
-            task_number = int(line[0])
+            task_id = int(line[0])
             duration = int(line[1])
             predecessors = line[2:]
-            tasks.append(Task(task_number, duration, *predecessors))
+            tasks.append(Task(task_id, duration, *predecessors))
 
     return tasks
 
@@ -35,7 +43,7 @@ Tasks = tasks_from_constraints("constraints/1.txt")
 
 for task in Tasks:
     print(
-        "task number:", task.task_number,
+        "task number:", task.task_id,
         "task.duration:", task.duration,
         "task.predecessors:", task.predecessors
     )
